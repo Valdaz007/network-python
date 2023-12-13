ce19d01fe16fa0b61dd5ee73016692f8ef29dc69
Victor <volsavaivictor@gmail.com>
2023-12-13 16:05:27 +1000

    Added Colorama Module

--- ping-v2/ping.py
+++ ping-v2/ping.py
@@ -1,4 +1,6 @@
-import os,csv
+import os,csv, colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
 def main():
     with open("cust-ip.csv", "r") as f:
@@ -8,8 +10,10 @@ def main():
         for row in rd:
             ping(row[0], row[1])
 
-def ping(name:str, ip: str):
-    os.system(f"echo 'Pinging {name}'")
+def ping(name:str, ip:str):
+    os.system("echo ''")
+    os.system("echo ''")
+    os.system(f"echo {Fore.RED}{name}{Fore.GREEN}")
     os.system(f"ping {ip}")
 
 if __name__ == "__main__":
