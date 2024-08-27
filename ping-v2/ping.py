82111f8308c010e8d3aa5a069f1d6c5b2b1c7ca9
Victor <volsavaivictor@gmail.com>
2024-08-27 13:37:23 +1000

    Update: Include Subprocess Module | Rmv: Past Clients

--- ping-v2/ping.py
+++ ping-v2/ping.py
@@ -1,9 +1,10 @@
+import subprocess
 import os, csv, colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
 def main():
-    with open("cust-ip.csv", "r") as f:
+    with open("cust-ip.csv", mode="r") as f:
         rd = csv.reader(f)
 
         next(rd)
@@ -11,10 +12,11 @@ def main():
             ping(row[0], row[1])
 
 def ping(name:str, ip:str):
-    os.system("echo ''")
-    os.system("echo ''")
-    os.system(f"echo {Fore.RED}{name}{Fore.GREEN}")
-    os.system(f"ping {ip}")
+    os.system(f"echo {Fore.YELLOW}*")
+    os.system(f"echo *")
+    os.system(f"echo * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+    os.system(f"echo {Fore.MAGENTA}{name}: {Fore.WHITE}{ip}{Fore.CYAN}")
+    subprocess.run(f'ping {ip}')
 
 if __name__ == "__main__":
     main()
