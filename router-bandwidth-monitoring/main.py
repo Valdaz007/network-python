96e656af9bed774ddb56b1119b7a9542fbbc09aa
Victor <volsavaivictor@gmail.com>
2024-08-28 09:59:29 +1000

    Update: Add Comments

--- router-bandwidth-monitoring/main.py
+++ router-bandwidth-monitoring/main.py
@@ -1,15 +1,21 @@
 from netmiko import ConnectHandler  # type: ignore
 import variables # Sensitive Variable Files
-# from pprint import pprint as prnt
 
 def main():
-    rates = showBandwidth()
-    print(rates)
-    print(f'RX: {convertTo(int(rates['rx']))} mpbs')
-    print(f'TX: {convertTo(int(rates['tx']))} mpbs')
+    rates = getBandwidth()  # Get Bandwidth Rates
 
-def showBandwidth() -> dict:
-    rtr = {
+    print(f'RX: {convertTo(int(rates['rx']))} mpbs') # Print Rx Rate in MBPS
+    print(f'TX: {convertTo(int(rates['tx']))} mpbs') # Print Tx Rate in MBPS
+
+def getBandwidth() -> dict:
+    # Function Flow: 
+    # Create SSH Connection with Targeted Router
+    # Send Command to Run
+    # Retrieve Command Results & Disconnect SSH Connection
+    # Filter Results for Input & Output Rates
+    # Return Rates
+
+    rtr = {     # Router Connection Info Dict
         'ip': variables.ip,
         'device_type': 'cisco_ios',
         'username': variables.username,
@@ -19,21 +25,26 @@ def showBandwidth() -> dict:
     }
 
     try:
-        conn = ConnectHandler(**rtr)
-        conn.enable()
-        intf = conn.send_command('sh int g8', use_textfsm=True)
-        intf = {"rx": intf[0]['input_rate'], "tx": intf[0]['output_rate']} 
-        conn.disconnect()
-        return intf
+        conn = ConnectHandler(**rtr)    # Create an SSH Connection with Router
+        conn.enable()                   
+        intf = conn.send_command('sh int g8', use_textfsm=True) # Send Command to Router
+        intf = {"rx": intf[0]['input_rate'], "tx": intf[0]['output_rate']} # Get Input & Output Rates
+        conn.disconnect()   # Close SSH Connection
+        return intf     # Return Rates
+    
     except Exception as e:
         return {"error":e}
 
 def convertTo(num:int, rate:str='mbps') -> int:
+    # Function Flow
+    # Get Rates As bits/s from Param (num) & Conversion Rate from Param (rate)
+    # Check Convert Rate & Convert
+    # Return New Rates
+
     if (rate == 'mbps'): return num/1000000
     elif (rate == 'gbps'): return num/1000000000
     elif (rate=='kbps'): return num/1000
     else: return 0
-
 
 if __name__ == "__main__":
     main()
